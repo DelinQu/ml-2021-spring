65a9923e4fef77ed0d0dfa156967aace3a8f9fcd
Delin Qu <qdl.cs@qq.com>
2021-12-19 22:38:36 +0800

    Update utils.py
    add time seed

--- Introduction/hw1-regression/utils.py
+++ Introduction/hw1-regression/utils.py
@@ -9,6 +9,18 @@ def get_device():
     return 'cuda' if torch.cuda.is_available() else 'cpu'
 
 '''
+fix random seed
+'''
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  
+    np.random.seed(seed)  
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+'''
 plot learning loss curve
 '''
 def plot_learning_curve(loss_record, title=''):
