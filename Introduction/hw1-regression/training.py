c5ca5fea61de4efb59e7d53fd6f9573f22a796b4
Delin Qu <qdl.cs@qq.com>
2021-12-20 08:14:37 +0800

    Update training.py
    fix a training error

--- Introduction/hw1-regression/training.py
+++ Introduction/hw1-regression/training.py
@@ -25,21 +25,21 @@ def train(tr_set, dv_set, model:NeuralNet, config:dict, device):
             optimizer.step()                        # update model with optimizer
             loss_record['train'].append(loss.detach().cpu().item())
 
-            # test model on validation (development) set after a epoch.
-            dev_loss = calc_dev_loss(dv_set, model, device)
-            if dev_loss < min_loss:
-                min_loss = dev_loss
-                print('Saving model (epoch = {:4d}, loss = {:.4f})'.format(epoch + 1, min_loss))
-                torch.save(model.state_dict(),config['save_path'])
-                early_stop_cnt = 0
-            else:
-                early_stop_cnt += 1
+        # test model on validation (development) set after a epoch.
+        dev_loss = calc_dev_loss(dv_set, model, device)
+        if dev_loss < min_loss:
+            min_loss = dev_loss
+            print('Saving model (epoch = {:4d}, loss = {:.4f})'.format(epoch + 1, min_loss))
+            torch.save(model.state_dict(),config['save_path'])
+            early_stop_cnt = 0
+        else:
+            early_stop_cnt += 1
 
-            epoch += 1
-            loss_record['dev'].append(dev_loss)
-            # stop training if model stops improving for early_stop epochs.
-            if early_stop_cnt > config['early_stop']:
-                break
+        epoch += 1
+        loss_record['dev'].append(dev_loss)
+        # stop training if model stops improving for early_stop epochs.
+        if early_stop_cnt > config['early_stop']:
+            break
 
     print('Finished training after {} epochs'.format(epoch))
     return min_loss, loss_record
